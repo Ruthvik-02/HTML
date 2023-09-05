d729be904f5497a541ba07c015882d8b17f5c352
Ruthvik-02 <ruthvikgowda07@gmail.com>
2023-09-05 23:31:36 +0530

    Day-10 programs done

---
+++ Day-10/javscript.js
@@ -0,0 +1,38 @@
+var name = "ruthvik";
+var age = 23;
+var weight = 50.66;
+
+var total =  age + 56 * 2;
+
+console.log(total);
+console.log(name + "ffkfffff")
+
+
+const r = 10;
+// r = 122;
+console.log(r);
+
+let a = 110;
+let b = 30;
+
+c = a+b;
+d = a/b;
+console.log(c);
+
+console.log(d);
+console.log(parseInt(d));
+// hh = Math.floor(d);
+console.log(d);
+
+for (let i = 0; i < 6; i++) {
+   console.log(i);
+}
+
+let mydata = {
+    name: 'ravi',
+    address: "sssssssss",
+    age :70,
+    score: [1,2,3,4]
+
+}
+console.log(mydata);
---
+++ Day-10/script.js
@@ -0,0 +1,24 @@
+/*function hii(a,b) {
+    console.log("ravi");
+    v=a+b;
+    return v;
+}
+
+let frist =10,second=15;
+let x= hii(frist, second)
+// let x = hii(10,15);
+
+console.log(x);*/
+
+
+
+function hii(a,b) {
+    console.log("ravi");
+
+    let c = prompt('enter the value of c');
+    alert("sum is" + " "+ a + b +c);
+}
+
+let x = hii(10,15);
+
+console.log(x);
