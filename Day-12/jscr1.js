c20fed6c24f25b8d31f9969bca5044979f57daa5
Ruthvik-02 <ruthvikgowda07@gmail.com>
2023-09-07 23:23:50 +0530

    Day-12 programs done

---
+++ Day-12/jscr1.js
@@ -0,0 +1,27 @@
+let x = 9;
+let y = 1;
+let z = x;
+
+console.log(`x ${x +91 /2},y${y},z${z}`);
+
+
+let names =`ravi`;
+let age = 19;
+
+let res =` my name is ${names + 'kumer'} and i am ${age} years old`;
+var length = `length ${names.length}`;  
+
+console.log(res);
+
+console.log(length);
+
+
+let a = 99999999998888998999991n;
+let b = BigInt("9999997777999299999959");
+console.log(`a ${a}+ b${b}`);
+
+const arr =[`ravi`,'raju',"ram"];
+console.log(arr[1]);
+
+console.log(arr.push("ramu"));
+console.log(arr[3]);
