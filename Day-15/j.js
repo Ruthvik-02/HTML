8c311868457c1aa83d326b3842363ff31a5e6930
Ruthvik-02 <ruthvikgowda07@gmail.com>
2023-09-10 23:19:15 +0530

    Day-15 programs done

---
+++ Day-15/j.js
@@ -0,0 +1,14 @@
+let heading1 = document.getElementsByTagName("h1")[0];
+
+let heading2 = document.getElementsByTagName()("h1")[1];
+
+heading1.innerText = "<p>Hello world</p>";
+
+heading2.innerHTML = "<u><marquee>Hello world</marquee></su>";
+
+function fun() {
+    let Image = document.getElementsByTagName("img")[0];
+
+    Image.width = "100px";
+    
+}
